3ef7d1519d98825d4da79333637554f9aa597c3b
Simon Boisvert <bois24@uqo.ca>
2018-03-11 14:52:20 -0400

    Ajout de commentaires

--- EmailParser/src/Parser/Parse.java
+++ EmailParser/src/Parser/Parse.java
@@ -9,16 +9,20 @@ import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
 
+/**
+ * Classe principale de l'application pour lancer le parseur
+ * Contient notre classe main lancé à l'exécution
+ */
 public class Parse {
 
 	public static void main(String[] args) throws MessagingException, IOException {
-		String m = "/Users/simon/Desktop/test2.eml";
-		Session s = Session.getInstance(new Properties());
-		InputStream mailFileInputStream = new FileInputStream(m);
-		MimeMessage message = new MimeMessage(s, mailFileInputStream);
+		String fichier = "/Users/simon/Desktop/test2.eml"; //Chemin du fichier à parser
+		Session s = Session.getInstance(new Properties()); 
+		InputStream mailFileInputStream = new FileInputStream(fichier); //Création d'un FileInputStream avec notre fichier
+		MimeMessage message = new MimeMessage(s, mailFileInputStream); //Instanciation de l'objet à parser
 		
-		MailList mail = new MailList();
-		mail = mail.getContent(message);
+		MailList mail = new MailList(); //Création d'une liste de courriels
+		mail = mail.getContent(message); //Ajout de notre courriel à la liste
 		
 		System.out.println(mail.date);
 		System.out.println(mail.from);
