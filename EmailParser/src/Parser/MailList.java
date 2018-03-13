3741c6f531441f0d791f13981f825b10d30a514f
Simon Boisvert <bois24@uqo.ca>
2018-03-13 18:45:19 -0400

    Ajout de certaines fonctions et classe MailList
    Ajout des fonctions pour :
    -charger un message
    -écrire un fichier
    
    Ajout de la classe MailList
    Changement de l'ancienne classe MailList à Mail

--- EmailParser/src/Parser/MailList.java
+++ EmailParser/src/Parser/MailList.java
@@ -1,86 +1,16 @@
 package Parser;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.mail.Address;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-
-import org.jsoup.Jsoup;
-
-/**
- * Classe permettant de garder une liste de courriels et pouvoir les parser
- *
- */
 public class MailList {
-
-	String from;
-	String subject;
-	String body;
-	String date;
-	ArrayList<MimeBodyPart> attachments;
 	
-	public MailList(){
-		
+	ArrayList<Mail> list;
+	
+	public MailList() {
+		list = new ArrayList<Mail>();
 	}
-
-	//Constructeur principale
-	public MailList(String from, String subject, String body, String date, ArrayList<MimeBodyPart> attachments) {
-		this.from = from;
-		this.subject = subject;
-		this.body = body;
-		this.date = date;
-		this.attachments = attachments;
-	}
-
-	//Méthode permettant de séparer les champs principales d'un courriel:
-	//Body, From, Date, Subject & Attachments
-	public MailList getContent(MimeMessage message) throws MessagingException, IOException {
-		String body = ""; //Initialisation
-		String from = "";
-		ArrayList<MimeBodyPart> attachments = new ArrayList<MimeBodyPart>(); //Création d'une liste pour les attachements
-		String contentType = message.getContentType(); //Variable permettant d'identifer le type de contenu lu
-		Address[] addresses = message.getFrom(); //Tableau contenant les addresses courriels de l'auteur du courriel
-		
-		//Provient d'une seule source
-		if (addresses.length == 1)
-			from = addresses[0].toString();
-		//Provient de plusieurs sources
-		else {
-			for (int num = 0; num < addresses.length - 1; num++)
-				from += addresses[num].toString() + ", ";
-			from += addresses[addresses.length].toString();
-		}
-		//Courriel formaté en texte simple
-		if (contentType.contains("TEXT/PLAIN") || contentType.contains("text/plain")) {
-			Object content = message.getContent();
-			if (content != null)
-				body += content.toString(); 
-		//Courriel formaté en HTML
-		} else if (contentType.contains("TEXT/HTML") || contentType.contains("text/html")) {
-			Object content = message.getContent();
-			if (content != null)
-				body += Jsoup.parse((String) content).text();
-		//Courriel fomaté en multipart
-		} else if (contentType.contains("multipart")) {
-			Multipart mp = (Multipart) message.getContent();
-			int numParts = mp.getCount();
-			for (int count = 0; count < numParts; count++) {
-				MimeBodyPart part = (MimeBodyPart) mp.getBodyPart(count);
-				String content = part.getContent().toString();
-				//Quand on identifie un attachement, on l'ajoute
-				if (MimeBodyPart.ATTACHMENT.equalsIgnoreCase(part.getDisposition()))
-					attachments.add(part);
-				//Sinon, on ajoute le reste
-				else if (part.getContentType().contains("TEXT/HTML") || contentType.contains("text/html"))
-					body += Jsoup.parse(content).text();
-				else
-					body += content;
-			}
-		}
-		return new MailList(from, message.getSubject(), body, message.getSentDate().toString(), attachments);
+	
+	public void ajoutMail(Mail mail){
+		list.add(mail);
 	}
 }
--- EmailParser/src/Parser/Parse.java
+++ EmailParser/src/Parser/Parse.java
@@ -1,8 +1,11 @@
 package Parser;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.Properties;
 
 import javax.mail.MessagingException;
@@ -16,19 +19,57 @@ import javax.mail.internet.MimeMessage;
 public class Parse {
 
 	public static void main(String[] args) throws MessagingException, IOException {
-		String fichier = "/Users/simon/Desktop/test2.eml"; //Chemin du fichier à parser
+		String fichier = "/Users/simon/Desktop/Courriels/test5.eml"; //Chemin du fichier à parser
+		MailList ml = new MailList();
+		
+		MimeMessage message = chargerMessage(fichier);
+	
+		Mail mail = new Mail(); //Création d'un courriel parsé
+		mail = mail.getContent(message); 
+		
+		ml.ajoutMail(mail); //Ajout de notre courriel à la liste
+		
+		ecrireFichier(ml);
+		
+	}
+	
+	
+	public static MimeMessage chargerMessage(String fichier) throws MessagingException, FileNotFoundException{
 		Session s = Session.getInstance(new Properties()); 
 		InputStream mailFileInputStream = new FileInputStream(fichier); //Création d'un FileInputStream avec notre fichier
-		MimeMessage message = new MimeMessage(s, mailFileInputStream); //Instanciation de l'objet à parser
-		
-		MailList mail = new MailList(); //Création d'une liste de courriels
-		mail = mail.getContent(message); //Ajout de notre courriel à la liste
-		
-		System.out.println(mail.date);
-		System.out.println(mail.from);
-		System.out.println(mail.subject);
-		System.out.println(mail.body);
-		System.out.println(mail.attachments.isEmpty());
-		
+		return new MimeMessage(s, mailFileInputStream); //Instanciation de l'objet à parser
+	}
+	
+	public static void ecrireFichier(MailList ml) throws FileNotFoundException{
+		  PrintWriter pw = new PrintWriter(new File("/Users/simon/Desktop/Courriels/data/data.csv"));
+		  StringBuilder sb = new StringBuilder();
+	      sb.append('"');
+	      sb.append((ml.list.get(0).date));
+	      sb.append('"');
+	      sb.append(',');
+	      
+	      sb.append('"');
+	      sb.append((ml.list.get(0).from));
+	      sb.append('"');
+	      sb.append(',');
+	      
+	      sb.append('"');
+	      sb.append((ml.list.get(0).subject));
+	      sb.append('"');
+	      sb.append(',');
+	      
+	      sb.append('"');
+	      sb.append((ml.list.get(0).body));
+	      sb.append('"');
+	      sb.append(',');
+	      
+	      sb.append('"');
+	      sb.append((ml.list.get(0).attachments.isEmpty()));
+	      sb.append('"');
+	      sb.append('\n');
+
+	      pw.write(sb.toString());
+	      pw.close();
+	      System.out.println("done!");
 	}
 }
